1278ed5debbd1dd8e0e3446a335a82de8fd153b3
Todinho <gabrieltosi5@gmail.com>
2019-05-14 19:05:13 -0300

    teste1

--- src/teste/teste.java
+++ src/teste/teste.java
@@ -3,8 +3,8 @@ package teste;
 public class teste {
 
 	public static void main(String[] args) {
-		System.out.println("Teste");
-
+		System.out.println("Teste");		
+		System.out.println("Teste1");
 	}
 PENIS
 }
